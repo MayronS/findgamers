b5cde811eefb8fd0ce4cc5005f59e3ff5ac91e43
Cássio Ávila <cassioavila@protonmail.com>
2024-11-03 22:22:35 -0300

    Adiciona a tela de perfil

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './Components/Home/Home'
 import Feed from './Components/Feed/Feed'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Chats from './Components/Chats/Chats'
+import ProfilePage from './Components/Profile/Profile'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/feed" element={<Feed />} />
         <Route path="/chats" element={<Chats />} />
+        <Route path="/profile" element={<ProfilePage />} />
       </Routes>
     </BrowserRouter>
     // <Presentation/>
